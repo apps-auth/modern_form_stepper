081371e528c3345fefc3d6a4f3b741e17aab6318
apps-auth <admin@appsauth.com>
2021-09-23 11:07:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## A flutter stepper widget with customizations
---
+++ lib/modern_form_stepper.dart
@@ -0,0 +1,6 @@
+library modern_form_stepper;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+part 'src/modern_form_stepper.dart';
